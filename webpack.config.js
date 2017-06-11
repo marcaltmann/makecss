4487c5b719e7bd693b15db3f4869e07885def7c8
Marc Altmann <marcaltmann@posteo.de>
2017-06-11 19:03:14 +0200

    Get webpack working

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name]-bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+  ],
+};
